f3b7ef8585a893a4e461d3f4d6e0475bdfd3dbe8
ayumi043 <ayumi043@gmail.com>
2016-07-01 23:40:33 +0800

    开始增加hub功能

--- Bilin3d/Modules/HubModule.cs
+++ Bilin3d/Modules/HubModule.cs
@@ -22,58 +22,15 @@ using System.IO;
 using System.Threading.Tasks;
 
 namespace Bilin3d.Modules {
-    public class OrderModule : BaseModule {
-        public OrderModule(IDbConnection db, ILog log, IRootPathProvider pathProvider)
-            : base("/order") {
+    public class HubModule : BaseModule {
+        public HubModule(IDbConnection db, ILog log, IRootPathProvider pathProvider)
+            : base("/hub") {
 
             this.RequiresAuthentication();
 
             Get["/"] = parameters => {
-                string stateid = Request.Query["state"].Value;
-                if (stateid != null) {
-                    if (!Regex.IsMatch(stateid, @"^[1-9]\d*?$")) {
-                        throw new Exception("stateid只能是大于0的数字");
-                        //return null;
-                    }
-                }
-
-                string condition = " and 1=1 ";
-                if (stateid != null) {
-                    condition = string.Format(@" and t3.Id='{0}' ", stateid);
-                }
-
-                var orderStates = db.Select<OrderStateModel>(string.Format(@"
-                    select id,statename from t_orderstate"));
-                var orders = db.Select<OrderModel>(string.Format(@"
-                    select t1.OrderId,
-                        t1.Express,
-                        t1.CreateTime,
-                        t1.Amount,
-                        t2.Area,
-                        t2.Size,
-                        t2.Volume,
-                        t2.Weight,
-                        t2.FileName,
-                        t2.Num,
-                        t5.name as MatName,
-                        t3.StateName,
-                        t3.Id as StateId,
-                        t4.Consignee,
-                        t4.Address 
-                    from t_order  t1
-                    left join t_orderdetail  t2 on t2.OrderId=t1.OrderId
-                    left join t_orderstate   t3 on t3.Id=t1.StateId
-                    left join t_address  t4 on t4.Id=t1.AddressId
-                    left join t_material  t5 on t5.MaterialId=t2.MaterialId
-                    where t1.UserId='{0}' {1}
-                    order by t1.CreateTime desc", Page.UserId, condition))
-                    //.GroupBy(i => new { i.OrderId, i.CreateTime, i.Consignee, i.StateName })
-                    .GroupBy(i => i.OrderId)
-                    .ToDictionary(k => k.Key, v => v.ToList());
-                base.Page.Title = "我的订单";
-                base.Model.OrderStates = orderStates;
-                base.Model.Orders = orders;
-                return View["Index", base.Model];
+               
+                return View["Index", Model];
             };
 
             Post["/"] = parameters => {
