78e444b571af861e7feaea2fce526fa1f8bfedd6
ayumi043 <ayumi043@gmail.com>
2016-08-04 18:32:05 +0800

    无
    无。

--- Bilin3d/Modules/HomeModule.cs
+++ Bilin3d/Modules/HomeModule.cs
@@ -10,6 +10,8 @@ using ServiceStack.OrmLite;
 using log4net;
 
 using Nancy.Validation;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace Bilin3d.Modules {
     public class HomeModule : BaseModule {
@@ -54,6 +56,22 @@ namespace Bilin3d.Modules {
                 return Response.AsRedirect("/bilinadmin/");
             };
 
+            Get["/hubs.js"] = _ => {
+                //string str = @"var data_info = [
+                //    [116.417854, 39.921988, '地址：北京市东城区王府井大街88号乐天银泰百货八层'],
+                //    [116.406605, 39.921585, '地址：北京市东城区东华门大街'],
+                //    [116.412222, 39.912345, '地址：北京市东城区正义路甲5号'],
+                //    [118.038, 25.472, '地址：福建省xx市有限公司'],
+                //    [94.29, 35.747, '地址：福建省xx市有限公司']
+                //];";
+                string str = "var data_info = [";
+                var suppliers = db.Select<SupplierModel>("SELECT Lng,Lat,Fname FROM t_supplier WHERE State='0' and (Lng <> 0 or Lng <> 0)");
+                suppliers.ForEach(i => {
+                    str = str + $@"[{i.Lng},{i.Lat},'{i.Fname}'],";
+                });
+                str = str.TrimEnd(',') + "];";
+                return Response.AsText(str);               
+            };
         }
     }
 }
